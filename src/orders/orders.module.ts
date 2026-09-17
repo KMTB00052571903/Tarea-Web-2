c46352c16a82b4331e11686cec969f91b26f7622
Juan Esteban Pérez <43039970+juanez1999@users.noreply.github.com>
2026-09-17 18:51:04 -0500

    Add files via upload

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { OrdersService } from "./orders.service";
+import { OrdersController } from "./orders.controller";
+import { OrderEntity } from "./entities/order.entity";
+import { CustomerEntity } from "./entities/customer.entity";
+import { TypeOrmModule } from "node_modules/@nestjs/typeorm/dist/typeorm.module";
+import { DiningEntity } from "./entities/dining.entity";
+import { OrderRulesService } from "./order-rules/order-rules.service";
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([OrderEntity, CustomerEntity, DiningEntity]),
+  ],
+  providers: [OrdersService, OrderRulesService],
+  controllers: [OrdersController],
+})
+export class OrdersModule {}
